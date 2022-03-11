1ba6db221d2243b1d1dd0fef78d68a8cbd62da75
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-03-11 08:02:00 +0000

    [Add]end_user編集画面作成

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,13 +1,17 @@
 class Public::EndUsersController < ApplicationController
   
-  def mypage
-    @spots = Spot.all
-    @tags = Tag.all
-  end
-  
   def show
     @end_user = EndUser.find(params[:id])
     @spots = @end_user.spots.order(created_at: :desc).page(params[:page])
+  end
+  
+  def edit
+    @end_user = current_end_user
+  end
+  
+  def update
+    current_end_user.update(end_user_params)
+    redirect_to end_user_path(current_end_user)
   end
   
   def withdraw
@@ -16,4 +20,11 @@ class Public::EndUsersController < ApplicationController
     reset_session
     redirect_to root_path
   end
+  
+  private
+  
+  def end_user_params
+    params.require(:end_user).permit(:name, :introduction, :profile_image)
+  end
+  
 end
