9543ae15971d17da60894ded29ac5c3d705b1898
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-03-13 09:51:15 +0000

    [Update]フォロー/フォロワー機能の非同期通信化

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,15 +1,13 @@
 class Public::RelationshipsController < ApplicationController
   
   def create
-    end_user = EndUser.find(params[:end_user_id])
-    current_end_user.follow(end_user)
-    redirect_to request.referer
+    @end_user = EndUser.find(params[:end_user_id])
+    current_end_user.follow(@end_user)
   end
   
   def destroy
-    end_user = EndUser.find(params[:end_user_id])
-    current_end_user.unfollow(end_user)
-    redirect_to request.referer
+    @end_user = EndUser.find(params[:end_user_id])
+    current_end_user.unfollow(@end_user)
   end
   
   def followings
