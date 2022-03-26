af9bd8412dde3e98531a84c615cf42c1652584ef
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-03-26 06:51:38 +0000

    [Update]住所の番地を半角に変換

--- app/controllers/public/spots_controller.rb
+++ app/controllers/public/spots_controller.rb
@@ -8,7 +8,12 @@ class Public::SpotsController < ApplicationController
   end
 
   def create
-    @spot = Spot.new(spot_params)
+    # 住所の番地の全角を半角へ変換
+    address_changed = address_params
+    address_changed[:address] = address_changed[:address].tr("０-９", "0-9").gsub(/(?<=\d)[‐－―ー−](?=\d)/, "-")
+    
+    temp_spot = spot_params.merge(address_changed)
+    @spot = Spot.new(temp_spot)
     @spot.end_user_id = current_end_user.id
 
     # タグをチェックする
@@ -65,7 +70,10 @@ class Public::SpotsController < ApplicationController
   private
 
   def spot_params
-    params.require(:spot).permit(:introduction, :address, spot_images: [], tag_ids: [])
+    params.require(:spot).permit(:introduction, spot_images: [], tag_ids: [])
+  end
+  def address_params
+    params.require(:spot).permit(:address)
   end
 
   def ensure_guest_user
