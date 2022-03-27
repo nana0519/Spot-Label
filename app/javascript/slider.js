a114c270f6e547ffeeccbb606eacfd629614d379
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-03-27 08:22:43 +0000

    [Update]レイアウト修正

--- app/javascript/slider.js
+++ app/javascript/slider.js
@@ -2,6 +2,9 @@
 import $ from "jquery"
 import "slick-carousel"
 
-document.addEventListener("DOMContentLoaded", () => {
-  $('.slider').slick();
-})
+// リロードしないと動かなくなるため
+document.addEventListener("turbolinks:load", function () {
+  $('.slider').slick({
+    arrows: true
+  });
+});
