86dee1eaab34814395ea4129c9ef67e5f6f8ba74
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-04-10 14:31:38 +0000

    [UPDATE]通知機能の修正

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -54,10 +54,13 @@ class EndUser < ApplicationRecord
 
   # フォローの通知
   def create_notification_follow(current_end_user)
-    notification = current_end_user.active_notifications.new(
-      visited_id: id,
-      action: "follow"
-    )
-    notification.save
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ", current_end_user.id, id, "follow"])
+    if temp.blank?
+      notification = current_end_user.active_notifications.new(
+        visited_id: id,
+        action: "follow"
+      )
+      notification.save
+    end
   end
 end
