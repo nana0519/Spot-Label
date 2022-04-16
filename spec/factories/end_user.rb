5410379ab2ac8e8a2cd71ab7d7cc2c08f7711528
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-04-16 08:49:43 +0000

    [UPDATE]RSpec テストコードの更新

--- spec/factories/end_user.rb
+++ spec/factories/end_user.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :end_user do
     name {Faker::Name.name}
-    email {Faker::Internet.email}
+    email {Faker::Internet.unique.email}
     password {"000000"}
-    password_confirmation {"000000"} 
+    password_confirmation {"000000"}
   end
 end
--- spec/system/end_user_spec.rb
+++ spec/system/end_user_spec.rb
@@ -2,15 +2,32 @@ require "rails_helper"
 
 describe "EndUsersコントローラに関するテスト" do
   let(:end_user) { FactoryBot.create(:end_user) }
-  
+
   # before do
-  #   visit new_end_user_registration_path
-  #   fill_in 'end_user[name]', with: end_user.name
-  #   fill_in 'end_user[email]', with: end_user.email
-  #   fill_in 'end_user[password]', with: end_user.password
-  #   fill_in 'end_user[password_confirmation]', with: end_user.password_confirmation
-  #   click_button "新規登録"
+  #   visit new_end_user_session_path
+  #   fill_in "end_user[email]", with: end_user.email
+  #   fill_in "end_user[password]", with: end_user.password
+  #   click_button "ログイン"
   # end
+  
+  before do
+    visit new_end_user_registration_path
+    fill_in "end_user[name]", with: end_user.name
+    fill_in "end_user[email]", with: end_user.email
+    fill_in "end_user[password]", with: end_user.password
+    fill_in "end_user[password_confirmation]", with: end_user.password_confirmation
+    click_button "新規登録"
+  end
+  
+  it "test" do
+    byebug 
+  end
+  # context "[Action]show" do
+  #   before do
+  #     visit end_user_path(end_user)
+  #   end
+  #   it "current_end_userの場合は編集ボタンが表示される" do
+      
 
   context "[Action]edit" do
     before do
@@ -21,6 +38,31 @@ describe "EndUsersコントローラに関するテスト" do
       expect(page).to have_field "end_user[email]", with: end_user.email
       expect(page).to have_field "end_user[introduction]", with: end_user.introduction
     end
+    it "更新処理に成功し、マイページへ遷移する" do
+      fill_in "end_user[name]", with: Faker::Name.name
+      fill_in "end_user[email]", with: Faker::Internet.email
+      fill_in "end_user[introduction]", with: "こんにちは！"
+      click_button "変更"
+      expect(page).to have_current_path end_user_path(end_user)
+    end
+    it "退会リンクを押すと退会確認画面へ遷移する" do
+      click_on "退会"
+      expect(page).to have_current_path unsubscribe_end_users_path
+    end
   end
 
+  context "[Action]withdraw" do
+    before do
+      visit unsubscribe_end_users_path
+    end
+    it "退会しないリンクを押すとspot一覧画面へ遷移する" do
+      click_on "退会しない"
+      expect(page).to have_current_path spots_path
+    end
+    it "退会するリンクを押すと退会する" do
+      click_on "退会する"
+      expect(end_user.reload.is_deleted).to eq true
+      expect(page).to have_current_path root_path
+    end
+  end
 end
